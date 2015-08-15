aab91a06a764117e94511eba9f2a55a403f145f4
hanssens <github@hanssens.com>
2015-08-15 13:20:53 +0200

    Removed the from-to overload, e.g. ToDataSourceResponse<TFrom, TTo>()

--- source/OpenKendoBinder/QueryableExtensions/EnumerableExtensions.cs
+++ source/OpenKendoBinder/QueryableExtensions/EnumerableExtensions.cs
@@ -10,10 +10,5 @@ namespace OpenKendoBinder.QueryableExtensions
         {
             return new DataSourceResponse<TModel>(request, query.AsQueryable());
         }
-
-        public static DataSourceResponse<TEntity, TViewModel> ToDataSourceResponse<TEntity, TViewModel>(this IEnumerable<TEntity> query, BaseDataSourceRequest request, IEnumerable<string> includes = null, Dictionary<string, string> mappings = null, Func<IQueryable<TEntity>, IEnumerable<TViewModel>> conversion = null, bool canUseAutoMapperProjection = true)
-        {
-            return new DataSourceResponse<TEntity, TViewModel>(request, query.AsQueryable(), includes, mappings, conversion, canUseAutoMapperProjection);
-        }
     }
 }
