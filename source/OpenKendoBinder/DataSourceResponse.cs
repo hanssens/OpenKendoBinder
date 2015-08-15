5ed26cd69dced589d97378d71ad95ea6a13c1a6c
hanssens <github@hanssens.com>
2015-08-15 21:13:22 +0200

    Refactored the removal of the AutoMapping functionality - Includes removing the DataSourceResponse<TEntity, TViewModel> overload - Includes removing all mapping references

--- source/OpenKendoBinder/DataSourceResponse.cs
+++ source/OpenKendoBinder/DataSourceResponse.cs
@@ -2,54 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Dynamic;
-using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using OpenKendoBinder.AutoMapperExtensions;
 using OpenKendoBinder.Containers;
 using OpenKendoBinder.Containers.Json;
 using OpenKendoBinder.Extensions;
 
 namespace OpenKendoBinder
 {
-    public class DataSourceResponse<TModel> : DataSourceResponse<TModel, TModel>
+    public class DataSourceResponse<TModel>
     {
-        public DataSourceResponse(BaseDataSourceRequest request, IQueryable<TModel> query)
-            : base(request, query, null, null)
-        {
-        }
-
-        public DataSourceResponse(BaseDataSourceRequest request, IEnumerable<TModel> list)
-            : this(request, list.AsQueryable())
-        {
-        }
-
-        public DataSourceResponse(IEnumerable<TModel> list, int totalCount)
-            : base(list, totalCount)
-        {
-        }
-    }
-
-    public class DataSourceResponse<TEntity, TViewModel>
-    {
-        private readonly Func<IQueryable<TEntity>, IEnumerable<TViewModel>> _conversion;
-        private readonly Dictionary<string, string> _mappings;
-        private readonly IQueryable<TEntity> _query;
+        private const string _TEntity = "TEntity__";
+        private readonly IQueryable<TModel> _query;
 
         public object Groups { get; set; }
-        public IEnumerable<TViewModel> Data { get; set; }
+        public IEnumerable<TModel> Data { get; set; }
         public object Aggregates { get; set; }
         public int Total { get; set; }
 
-        public DataSourceResponse(BaseDataSourceRequest request,
-            IQueryable<TEntity> query,
-            IEnumerable<string> includes = null,
-            Dictionary<string, string> mappings = null,
-            Func<IQueryable<TEntity>, IEnumerable<TViewModel>> conversion = null,
-            bool canUseAutoMapperProjection = true)
-        {
-            _mappings = AutoMapperUtils.GetModelMappings<TEntity, TViewModel>(mappings);
-            _conversion = conversion ?? GetAutoMapperConversion(canUseAutoMapperProjection);
+        public DataSourceResponse(BaseDataSourceRequest request, IEnumerable<TModel> list) : this(request, list.AsQueryable()) { }
 
+        public DataSourceResponse(BaseDataSourceRequest request, IQueryable<TModel> query) : this(request, query, null) { }
+
+        public DataSourceResponse(BaseDataSourceRequest request, IQueryable<TModel> query, IEnumerable<string> includes = null)
+        {
             IList<string> includesAsList = null;
             if (includes != null)
             {
@@ -87,48 +61,40 @@ namespace OpenKendoBinder
 
                 _query = tempQuery;
 
-                Data = _conversion(_query).ToList();
+                Data = _query.ToList();
                 Groups = null;
             }
         }
 
-        protected DataSourceResponse(BaseDataSourceRequest request, IEnumerable<TEntity> entities,
-            Dictionary<string, string> mappings,
-            Func<IQueryable<TEntity>, IEnumerable<TViewModel>> conversion
-            )
-            : this(request, entities.AsQueryable(), null, mappings, conversion)
-        {
-        }
-
-        protected DataSourceResponse(IEnumerable<TViewModel> list, int totalCount)
+        protected DataSourceResponse(IEnumerable<TModel> list, int totalCount)
         {
             Data = list;
             Total = totalCount;
         }
 
-        public IQueryable<TEntity> AsQueryable()
+        public IQueryable<TModel> AsQueryable()
         {
             return _query;
         }
 
-        private IQueryable<TEntity> ApplyFiltering(IQueryable<TEntity> query, FilterObjectWrapper filter)
+        private IQueryable<TModel> ApplyFiltering(IQueryable<TModel> query, FilterObjectWrapper filter)
         {
             var filtering = GetFiltering(filter);
             return filtering != null ? query.Where(filtering) : query;
         }
 
-        private IQueryable<TEntity> ApplySorting(IQueryable<TEntity> query, IEnumerable<SortObject> sortObjects)
+        private IQueryable<TModel> ApplySorting(IQueryable<TModel> query, IEnumerable<SortObject> sortObjects)
         {
             var sorting = GetSorting(sortObjects) ?? query.ElementType.FirstSortableProperty();
             return query.OrderBy(sorting);
         }
 
-        protected object ApplyAggregates(IQueryable<TEntity> query, IList<string> includes, BaseDataSourceRequest request)
+        protected object ApplyAggregates(IQueryable<TModel> query, IList<string> includes, BaseDataSourceRequest request)
         {
             // In case of average, sum, min or max: convert it to sum(TEntity__.XXX) as sum_XXX
             // In case of count, convert it to count() as count_XXX
             var convertedAggregateObjects = request.AggregateObjects
-                .Select(a => a.GetLinqAggregate(MapFieldfromViewModeltoEntity))
+                .Select(a => a.GetLinqAggregate())
                 .Distinct()
                 .ToList();
 
@@ -139,18 +105,18 @@ namespace OpenKendoBinder
 
             if (includes != null && includes.Any())
             {
-                includesX = ", " + string.Join(", ", includes.Select(i => "it." + i + " as TEntity__" + i.Replace(".", "_")));
+                includesX = ", " + string.Join(", ", includes.Select(i => "it." + i + " as " + _TEntity + i.Replace(".", "_")));
             }
 
             // Execute the Dynamic Linq "Select" to get TEntity__ (and includes if needed). Also add a fake __Key__ property to allow grouping
-            // Example : new ("__Key__" as __Key__, it AS TEntity__, it.Company as TEntity__Company, it.Company.MainCompany as TEntity__Company_MainCompany, it.Country as TEntity__Country)
-            var selectTEntityQuery = query.Select(string.Format("new (\"__Key__\" as __Key__, it AS TEntity__{0})", includesX));
+            // Example : new ("__Key__" as __Key__, it AS TModel__, it.Company as TEntity__Company, it.Company.MainCompany as TEntity__Company_MainCompany, it.Country as TEntity__Country)
+            var selectTModelQuery = query.Select(string.Format("new (\"__Key__\" as __Key__, it AS " + _TEntity + "{0})", includesX));
 
             // Group by the __Key__ to allow aggregates to be calculated
-            var groupByQuery = selectTEntityQuery.GroupBy("__Key__");
+            var groupByQuery = selectTModelQuery.GroupBy("__Key__");
 
             // Execute the Dynamic Linq "Select" to add the aggregates
-            // Example : new (new (Sum(TEntity__.Id) as sum__Id, Min(TEntity__.Id) as min__Id, Max(TEntity__.Id) as max__Id, Count() as count__Id, Average(TEntity__.Id) as average__Id) as Aggregates)
+            // Example : new (new (Sum(TEntity__.Id) as sum__Id, Min(TEntity__.Id) as min__Id, Max(TModel__.Id) as max__Id, Count() as count__Id, Average(TEntity__.Id) as average__Id) as Aggregates)
             var aggregatesQuery = groupByQuery.Select(aggregatesExpression);
 
             // Try to get first result, cast to DynamicClass as use helper method to convert this to correct response
@@ -159,18 +125,18 @@ namespace OpenKendoBinder
             return aggregates;
         }
 
-        protected IEnumerable<DataSourceGroup> ApplyGroupingAndSorting(IQueryable<TEntity> query, IList<string> includes, BaseDataSourceRequest request)
+        protected IEnumerable<DataSourceGroup> ApplyGroupingAndSorting(IQueryable<TModel> query, IList<string> includes, BaseDataSourceRequest request)
         {
             bool hasAggregates = request.GroupObjects.Any(g => g.AggregateObjects.Any());
             string aggregatesExpression = string.Empty;
 
             if (hasAggregates)
             {
-                // In case of sum, min or max: convert it to sum(TEntity__.XXX) as sum_XXX
+                // In case of sum, min or max: convert it to sum(TModel__.XXX) as sum_XXX
                 // In case of count, convert it to count() as count_XXX
                 var convertedAggregateObjects = request.GroupObjects
                     .SelectMany(g => g.AggregateObjects)
-                    .Select(a => a.GetLinqAggregate(MapFieldfromViewModeltoEntity))
+                    .Select(a => a.GetLinqAggregate())
                     .Distinct()
                     .ToList();
 
@@ -182,19 +148,19 @@ namespace OpenKendoBinder
             bool hasSortObjects = sort.Any();
 
             // List[0] = LastName as Last
-            var groupByFields = request.GroupObjects.Select(s => string.Format("{0} as {1}", MapFieldfromViewModeltoEntity(s.Field), s.Field)).ToList();
+            var groupByFields = request.GroupObjects.Select(s => string.Format("{0} as {1}", s.Field, s.Field)).ToList();
 
             // new (new (LastName as Last) as GroupByFields)
             var groupByExpressionX = string.Format("new (new ({0}) as GroupByFields)", string.Join(",", groupByFields));
 
             // new (Key.GroupByFields, it as Grouping, new (sum(TEntity__.EmployeeNumber) as sum__TEntity___EmployeeNumber) as Aggregates)
             var selectExpressionBeforeOrderByX = string.Format("new (Key.GroupByFields, it as Grouping {0})", aggregatesExpression);
-            var groupSort = string.Join(",", request.GroupObjects.ToList().Select(s => string.Format("{0} {1}", MapFieldfromViewModeltoEntity(s.Field), s.Direction)));
+            var groupSort = string.Join(",", request.GroupObjects.ToList().Select(s => string.Format("{0} {1}", s.Field, s.Direction)));
 
             // Adam Downs moved sort to items vs group
             var orderByFieldsExpression = hasSortObjects ?
-                string.Join(",", sort.Select(s => string.Format("{0} {1}", MapFieldfromViewModeltoEntity(s.Field), s.Direction))) :
-                MapFieldfromViewModeltoEntity(request.GroupObjects.First().Field);
+                string.Join(",", sort.Select(s => string.Format("{0} {1}", s.Field, s.Direction))) :
+                request.GroupObjects.First().Field;
 
             // new (GroupByFields, Grouping, Aggregates)
             var selectExpressionAfterOrderByX = string.Format("new (GroupByFields, Grouping{0})", hasAggregates ? ", Aggregates" : string.Empty);
@@ -203,7 +169,7 @@ namespace OpenKendoBinder
             string includesX = string.Empty;
             if (includes != null && includes.Any())
             {
-                includesX = ", " + string.Join(", ", includes.Select(i => "it." + i + " as TEntity__" + i.Replace(".", "_")));
+                includesX = ", " + string.Join(", ", includes.Select(i => "it." + i + " as " + _TEntity + i.Replace(".", "_")));
             }
 
             var limitedQuery = query.OrderBy(string.Join(",", new[] { groupSort, orderByFieldsExpression }));
@@ -219,7 +185,7 @@ namespace OpenKendoBinder
             }
 
             // Execute the Dynamic Linq "GroupBy"
-            var groupByQuery = limitedQuery.GroupBy(groupByExpressionX, string.Format("new (it AS TEntity__{0})", includesX));
+            var groupByQuery = limitedQuery.GroupBy(groupByExpressionX, string.Format("new (it AS {1}{0})", includesX, _TEntity));
 
             // Execute the Dynamic Linq "Select"
             var selectQuery = groupByQuery.Select(selectExpressionBeforeOrderByX);
@@ -228,8 +194,8 @@ namespace OpenKendoBinder
             var orderByQuery = selectQuery.OrderBy(string.Join(",", request.GroupObjects
                 .Select(s => string.Format("GroupByFields.{0} {1}", s.Field, s.Direction)).ToList()));
 
-            // Execute the Dynamic Linq "Select" to get back the TEntity objects
-            var tempQuery = orderByQuery.Select(selectExpressionAfterOrderByX, typeof(TEntity));
+            // Execute the Dynamic Linq "Select" to get back the TModel objects
+            var tempQuery = orderByQuery.Select(selectExpressionAfterOrderByX, typeof(TModel));
 
             // Create a valid List<DataSourceGroup> object
             var list = new List<DataSourceGroup>();
@@ -262,8 +228,8 @@ namespace OpenKendoBinder
 
             if (isLast)
             {
-                var entities = grouping.Select<TEntity>("TEntity__").AsQueryable();
-                kendoGroup.items = _conversion(entities).ToList();
+                var entities = grouping.Select<TModel>(_TEntity).AsQueryable();
+                kendoGroup.items = entities.ToList();
             }
             else
             {
@@ -285,7 +251,7 @@ namespace OpenKendoBinder
                 return null;
             }
 
-            var expression = string.Join(",", sortObjects.Select(s => MapFieldfromViewModeltoEntity(s.Field) + " " + s.Direction));
+            var expression = string.Join(",", sortObjects.Select(s => s.Field + " " + s.Direction));
             return expression.Length > 1 ? expression : null;
         }
 
@@ -295,8 +261,8 @@ namespace OpenKendoBinder
 
             foreach (var filterObject in filter.FilterObjects)
             {
-                filterObject.Field1 = MapFieldfromViewModeltoEntity(filterObject.Field1);
-                filterObject.Field2 = MapFieldfromViewModeltoEntity(filterObject.Field2);
+                filterObject.Field1 = filterObject.Field1;
+                filterObject.Field2 = filterObject.Field2;
 
                 if (finalExpression.Length > 0)
                 {
@@ -305,14 +271,14 @@ namespace OpenKendoBinder
 
                 if (filterObject.IsConjugate)
                 {
-                    var expression1 = filterObject.GetExpression1<TEntity>();
-                    var expression2 = filterObject.GetExpression2<TEntity>();
+                    var expression1 = filterObject.GetExpression1<TModel>();
+                    var expression2 = filterObject.GetExpression2<TModel>();
                     var combined = string.Format("({0} {1} {2})", expression1, filterObject.LogicToken, expression2);
                     finalExpression += combined;
                 }
                 else
                 {
-                    var expression = filterObject.GetExpression1<TEntity>();
+                    var expression = filterObject.GetExpression1<TModel>();
                     finalExpression += expression;
                 }
             }
@@ -320,42 +286,6 @@ namespace OpenKendoBinder
             return finalExpression.Length == 0 ? "true" : finalExpression;
         }
 
-        protected string MapFieldfromViewModeltoEntity(string field)
-        {
-            return (_mappings != null && field != null && _mappings.ContainsKey(field)) ? _mappings[field] : field;
-        }
-
-        protected string MapFieldfromEntitytoViewModel(string field)
-        {
-            return (_mappings != null && field != null && _mappings.ContainsValue(field)) ? _mappings.First(kvp => kvp.Value == field).Key : field;
-        }
-
-        public static Func<IQueryable<TEntity>, IEnumerable<TViewModel>> GetAutoMapperConversion(bool canUseAutoMapperProjection = true)
-        {
-            Func<IQueryable<TEntity>, IEnumerable<TViewModel>> conversion;
-
-            if (AutoMapperUtils.SameTypes<TEntity, TViewModel>())
-            {
-                conversion = q => q.Cast<TViewModel>().ToList();
-            }
-            else
-            {
-                // https://github.com/AutoMapper/AutoMapper/issues/362
-                // The idea behind Project().To is to be passed to a query provider like EF or NHibernate that will then do the appropriate SQL creation, 
-                // not necessarily that the in-memory-execution will work.
-                // Project.To has a TON of limitations as it's built explicitly for real query providers, and only does things like MapFrom etc.
-                // To put it another way - don't use Project.To unless you're passing that to EF or NH or another DB query provider that knows what to do with the expression tree.
-                if (canUseAutoMapperProjection)
-                {
-                    conversion = q => Enumerable.AsEnumerable(q.Project().To<TViewModel>());
-                }
-                else
-                {
-                    conversion = Mapper.Map<IEnumerable<TViewModel>>;
-                }
-            }
-
-            return conversion;
-        }
     }
+
 }
